cfc0ef1e7b783fcf8c85994917f6f76d59b58886
Diego Sousa <diego@diegosousa.com.br>
2020-10-19 16:37:15 -0300

    Create Migration for user table

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,21 +3,21 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["airbnb-base", "prettier"],
-  plugins: ["prettier"],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
   rules: {
-    "prettier/prettier": "error",
-    "class-methods-use-this": "off",
-    "no-param-reassign": "off",
-    camelcase: "off",
-    "no-unused-vars": [
-      "error",
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': [
+      'error',
       {
-        argsIgnorePattern: "next",
+        argsIgnorePattern: 'next',
       },
     ],
   },
---
+++ src/database/migrations/20201019140934-create-users.js
@@ -0,0 +1,33 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.sequelize
+      .query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
+      .then(async () => {
+        return queryInterface.createTable('users', {
+          uuid: {
+            type: Sequelize.UUID,
+            allowNull: false,
+            primaryKey: true,
+            defaultValue: Sequelize.literal('uuid_generate_v4()'),
+          },
+          name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+          },
+          password_hash: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+        });
+      });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('users');
+  },
+};
