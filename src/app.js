820f1f0ac32c69f9f1d83fac045f887808db36b3
Diego Sousa <diego@diegosousa.com.br>
2020-10-19 05:54:14 -0300

    Creates code structure

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const routes = require('./routes');
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    /**
+     * Accepts JSON format
+     */
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+module.exports = new App().server;
