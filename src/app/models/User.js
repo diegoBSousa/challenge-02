8e34771b28f9bcb7d7fd5e4795de1b9f80e6c4ea
Diego Sousa <diego@diegosousa.com.br>
2020-10-26 06:44:55 -0300

    Uses bcrypt and uuid

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,22 +1,36 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+import { v4 as uuidv4 } from 'uuid';
 
 class User extends Model {
   static init(sequelize) {
     super.init(
       {
         uuid: {
-          type: Sequelize.UUID,
-          allowNull: false,
+          type: Sequelize.UUID, // 2:34 gerando hash
           primaryKey: true,
         },
         name: Sequelize.STRING,
         email: Sequelize.STRING,
         password_hash: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
       },
       {
         sequelize,
       }
     );
+
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.email + user.password, 8);
+      }
+    });
+
+    this.addHook('beforeCreate', async (user) => {
+      user.uuid = uuidv4();
+    });
+
+    return this;
   }
 }
 
